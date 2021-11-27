343b135741d08ae90d667dfedbb40142054ee0c9
Albert Mietus <albert AT mietus DOT nl>
2021-11-27 23:44:11 +0100

    asis

--- Arpeggio/pytst/test_4_rule.py
+++ Arpeggio/pytst/test_4_rule.py
@@ -4,27 +4,32 @@ from grammar import *
 import arpeggio
 RE, S = arpeggio.RegExMatch, arpeggio.StrMatch                          # shortcut
 
-show_dot=True
+show_dot=False
 
 def parse_rule(txt, pattern=None):
-    print(f'\nXXX >>{txt}<<')
     parser = ParserPython(rule, comment, debug=show_dot)
     tree = parser.parse(txt)
     assert tree.position_end == len(txt) , f"Not parsed whole input; Only: >>{txt[tree.position: tree.position_end]}<<; Not: >>{txt[tree.position_end:]}<<."
-
     print(f'\nTREE\n{tree.tree_str()}')
+    assert len(tree) == 4, 		      		"A rule should have length=4; ..."
+    assert tree[0].rule_name == "rule_name",  		"  at [0], the name of the rule"
+    assert str(tree[1]) == '<-',                        "  then a arrow"
+    assert tree[2].rule_name == "ordered_choice",	"  at [2] an ordered_choice"
+    assert str(tree[3]) == ';',                         "  and the the closing ':'"
 
-#    assert len(tree) == 3, "txt_term is an Ordered_Choice of always 3 Terminals"
-#    assert isinstance(tree.txt, arpeggio.OrderedChoice) and tree.txt_name == "rule_term"
-
-
-#    for e in tree: assert isinstance(e, arpeggio.Terminal)
-#    if pattern:
-#        for e,T in zip(tree,pattern):
-#            if T is not None: assert isinstance(e.rule, T), f"{type(e.rule).__name__}  doesn't match {T.__name__}"
+    print(f'\nTREE[2]=>>{tree[0]}<<\n{tree[2].tree_str()}')
 
     return tree
 
-def test_simple():	parse_rule(r"R <- A  B C ;")
 
+def test_simple():	parse_rule(r"R <- A  B C ;")
+def test_OC():		parse_rule(r"Alts <-  This | That | Or So ;")
+
+def test_rule_rule():	parse_rule(r"""RULE	           <-  RULE_NAME '<-' ORDERED_CHOICE ';' ;""")
+def test_expression_rule():	parse_rule(r"""
+   expression	   <- regex_term
+   		   |  rule_crossref
+		   |  '(' ordered_choice ')'
+		   |  str_term
+		   ;""")
 
