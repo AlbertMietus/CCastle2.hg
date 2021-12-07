cec5ebec9c0c5f123a36a9fd3b559832bee81614
Albert Mietus <albert AT mietus DOT nl>
2021-12-07 16:54:51 +0100

    Demo of gen_def's

--- Jinja/demo/demo1.py
+++ Jinja/demo/demo1.py
@@ -8,10 +8,23 @@ def demo_b():
     t = Template("My favorite numbers: {% for n in range(1,10) %}{{n}} " "{% endfor %}")
     print(t.render())
 
+def gen_rule(name, peg_expr_list):
+    if isinstance(peg_expr_list, str): peg_expr_list=[peg_expr_list]
+    return Template(
+        """def {{ name }}():
+        return {% for e in peg_expr_list -%}
+                  {{- e -}} 
+                  {{ ", " if not loop.last else "" -}}
+               {% endfor -%}\n\n""").render(locals())
+def demo_def1():
+   return gen_rule('peg_grammar', ['rules', 'EOF'])
+def demo_def2():
+   return gen_rule('rules', "OneOrMore(rule)")
 
 if __name__ == "__main__":
     demos  = [ func for (name, func) in locals().items()  if name.startswith('demo_') and callable(func) ]
     for demo in demos:
         print(f"{'*'*12}\n** {demo.__name__} ::\n{'*'*12}")
-        demo()
+        retval=demo()
+        if isinstance(retval, str): print(retval)
     print("== Done\n")
