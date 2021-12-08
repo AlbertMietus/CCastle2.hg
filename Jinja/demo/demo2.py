3b1289b86c10e738c73e278f65f69743ce529f72
Albert Mietus <albert AT mietus DOT nl>
2021-12-08 14:08:22 +0100

    cleanup

--- Jinja/demo/demo2.py
+++ Jinja/demo/demo2.py
@@ -7,7 +7,7 @@ class Demo1():
         template_dirs = [  self._get_file_dirPath() / d for d in template_dirs]
 
         templateLoader   = jinja2.FileSystemLoader(searchpath=template_dirs)
-        self.templateEnv = jinja2.Environment(loader=templateLoader)
+        self.templateEnv = jinja2.Environment(loader=templateLoader, trim_blocks=True, lstrip_blocks=True)
         self._load_tempate(default_template)
 
     def _load_tempate(self, file):
@@ -20,10 +20,11 @@ class Demo1():
         path_to_current_test = Path(os.path.realpath(__file__))
         path_to_current_dir = path_to_current_test.parent
         return path_to_current_dir
-        
+
     def render(self, *, template=None, **kwarsgs):
         if template: self._load_tempate(template)                       # ReLoad default_template
         return self.template.render(**kwarsgs)
+
 
 
 def demo_def1():
@@ -31,10 +32,15 @@ def demo_def1():
                          name='peg_grammar',
                          peg_expr_list=['rules', 'EOF'])
 
+def demo_def2():
+   return Demo1().render(template='peg_grammar',
+                         name='rules',
+                         peg_expr_list=['OneOrMore(rule)'])
+
 if __name__ == "__main__":
     demos  = [ func for (name, func) in locals().items()  if name.startswith('demo_') and callable(func) ]
     for demo in demos:
-        print(f"{'*'*12}\n** {demo.__name__} ::\n{'*'*12}")
+        #print(f"{'*'*12}\n** {demo.__name__} ::\n{'*'*12}")
         retval=demo()
         if isinstance(retval, str): print(retval)
-    print("== Done\n")
+    #print("== Done\n")
