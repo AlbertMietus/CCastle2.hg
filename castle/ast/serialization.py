0901c5724ecf5490303c6acb2378188e2684c5b4
Albert Mietus <albert AT mietus DOT nl>
2022-01-28 23:37:20 +0100

    Even more AST-2-XML serialization

--- castle/ast/serialization.py
+++ castle/ast/serialization.py
@@ -68,10 +68,13 @@ class XML_Serialize(Serialize):
         for elm in ast.value:
             self._ast2xml(elm, seq)
 
+
+    def Rule2xml(self, ast, parent) ->None:
+        rule = ET.SubElement(parent, 'Rule', name=ast.name.name)
+        for elm in ast.expr:
+            self._ast2xml(elm, rule)
+
 #############
-    def Rule2xml(self, ast, parent) ->None: ...
-
-
     def Rules2xml(self, ast, parent) ->None: ...
     def Setting2xml(self, ast, parent) ->None: ...
     def Settings2xml(self, ast, parent) ->None: ...
--- pytst/ast/XML_serialization/test_1_simple.py
+++ pytst/ast/XML_serialization/test_1_simple.py
@@ -23,17 +23,21 @@ def assert_xml_Element(txt, tag,
         logger.debug(f'XXX2 tag={tag}:: attrib={attrib}, value={value}')
         assert found.attrib[attrib] == value
 
+
 def test_ID(xml_serialize):
     txt = xml_serialize(peg.ID(name='demo_ID'))
     assert_xml_Element(txt, tag='ID', name='demo_ID')
+
 
 def test_StrTerm(xml_serialize):
     txt = xml_serialize(peg.StrTerm(value='demo string'))
     assert_xml_Element(txt, tag='StrTerm', value='demo string')
 
+
 def test_RegExpTerm(xml_serialize):
     txt = xml_serialize(peg.RegExpTerm(value='demo RegExp'))
     assert_xml_Element(txt, tag='RegExpTerm', value='demo RegExp')
+
 
 def test_Sequence_1(xml_serialize):
     e1 = peg.ID(name='ID_1')
@@ -42,7 +46,8 @@ def test_Sequence_1(xml_serialize):
     assert_xml_Element(txt, tag='Sequence')
     assert_xml_Element(txt, tag='.//ID', name='ID_1')
 
-def test_Sequence_1(xml_serialize):
+
+def test_Sequence_3(xml_serialize):
     n1, v2, v3 = 'ID_1', 'str_2', 'regexp_3'
     e1 = peg.ID(name=n1)
     e2 = peg.StrTerm(value=v2)
@@ -56,3 +61,31 @@ def test_Sequence_1(xml_serialize):
     assert_xml_Element(txt, tag='.//StrTerm', value=v2)
     assert_xml_Element(txt, tag='.//RegExpTerm', value=v3)
 
+
+def test_Rule_1ID(xml_serialize):
+    rule_name, xref = "RuleName", "cross_ref"
+    name = peg.ID(name=rule_name)
+    expr = peg.Sequence(value=[peg.ID(name=xref)])
+
+    txt = xml_serialize(peg.Rule(name=name, expr=expr))
+    logger.debug(f'XML:: {txt}')
+
+    assert_xml_Element(txt, tag='Rule', name=rule_name)
+    assert_xml_Element(txt, tag='.//ID', name=xref)
+
+
+def test_Rule_Sequence(xml_serialize):
+    rule_name = "RuleName"
+    name = peg.ID(name=rule_name)
+
+    n1, v2, v3 = 'ID_1', 'str_2', 'regexp_3'
+    e1 = peg.ID(name=n1)
+    e2 = peg.StrTerm(value=v2)
+    e3 = peg.RegExpTerm(value=v3)
+
+    expr = peg.Sequence(value=[e1, e2, e3])
+
+    txt = xml_serialize(peg.Rule(name=name, expr=expr))
+    logger.debug(f'XML:: {txt}')
+
+    assert_xml_Element(txt, tag='Rule', name=rule_name)
