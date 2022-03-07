2ef1ff6b7d3ef112c692d8980fb7e77626be4c40
Albert Mietus <albert AT mietus DOT nl>
2022-03-07 21:49:22 +0100

    test comments -- XXX test works, but file not, see XXX

---
+++ pytst/readers/parser/d1_parse_tree/test_6_comments.py
@@ -0,0 +1,20 @@
+import pytest
+import logging;logger = logging.getLogger(__name__)
+
+from castle.readers.parser import grammar
+
+import arpeggio
+RE, S = arpeggio.RegExMatch, arpeggio.StrMatch                          # shortcut
+
+from . import parse
+
+def test_comment_hash():
+    parse_tree=parse("""a = b ;# Comment\n""", grammar.peg_grammar)
+    logging.debug(f"parse_tree {parse_tree} -- no comments!")
+    assert True, "When comments (with hash) are parsed, it's fine"
+
+def test_comment_slash():
+    parse_tree=parse("""a = b ;// Comment\n""", grammar.peg_grammar)
+    logging.debug(f"parse_tree {parse_tree} -- no comments!")
+    assert True, "When comments (with slashed) are parsed, it's fine"
+
