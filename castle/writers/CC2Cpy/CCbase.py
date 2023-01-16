cbb036d9be96ce06b23c72527603e8e831387feb
Albert Mietus <albert AT mietus DOT nl>
2023-01-16 21:11:19 +0100

    mini-refactor
    --HG--
    branch : dev-writers

--- castle/writers/CC2Cpy/CCbase.py
+++ castle/writers/CC2Cpy/CCbase.py
@@ -9,7 +9,7 @@ fstring: TypeAlias=str # a Fix (sized) string. May be inplemented as a C-string,
 class CC_Base: pass
 
 @dataclass
-class CC_TypedParameter:
+class CC_TypedParameter(CC_Base):
     """This is many a helper class/struct to combine a parameter: a name and an type"""
     name: fstring
     type: type
--- castle/writers/CC2Cpy/Event.py
+++ castle/writers/CC2Cpy/Event.py
@@ -5,7 +5,7 @@ from dataclasses import dataclass, KW_ONLY
 from collections.abc import Sequence # Use for typing
 
 @dataclass
-class CC_Event:
+class CC_Event(CC_Base):
     """An event is like a (remote) function-call
 
        It has a name, a return-type (can be void), and a sequence of typed parameters."""
