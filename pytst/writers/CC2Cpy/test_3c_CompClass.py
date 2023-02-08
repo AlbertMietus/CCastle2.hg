dc75abfd4cea6b1bd30d8338c789228471555b6a
Albert Mietus <albert AT mietus DOT nl>
2023-02-08 22:53:33 +0100

    (asis) Refactor tests: ref_XX (txt) and fixtures/call to common.py. Some fixtures are now call()s
    --HG--
    branch : dev-writers

--- pytst/writers/CC2Cpy/test_3c_CompClass.py
+++ pytst/writers/CC2Cpy/test_3c_CompClass.py
@@ -8,9 +8,26 @@ from castle.writers.CC2Cpy.Component import * # CC_B_ComponentClass
 
 from . import common
 
-def test_0():
-    emptyClass = CC_B_ComponentClass(common.emptyComp())
-    assert CCompare(common.ref_emptyClass, emptyClass.render())
+def test_1a_render_empty():
+    compClass = CC_B_ComponentClass(common.emptyComp())
+    assert CCompare(common.ref_emptyClass, compClass.render())
+
+def test_1a_render_demo2():
+    compClass = CC_B_ComponentClass(common.demo2Comp())
+    assert CCompare(common.ref_demo2Class, compClass.render())
+
+def test_2_render_whitespace():
+    assert CCompare(common.ref_emptyClass, CC_B_ComponentClass(common.emptyComp()).render())
+    assert CCompare(common.ref_emptyClass, CC_B_ComponentClass(common.emptyComp()).render_struct())
+
+def test_3a_indent_empty():
+    verify_indents(common.ref_emptyComp, common.emptyComp().render)
+
+def test_3b_indent_demo():
+    verify_indents(common.ref_demo2Comp, common.demo2Comp().render)
+
+def test_3c_indent_sub():
+    verify_indents(common.ref_subComp, common.subComp(base=common.demo2Comp()).render)
 
 
 
