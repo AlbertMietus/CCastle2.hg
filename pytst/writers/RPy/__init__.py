c6ecf964b6fbc9ebd057face3f17082d2fdbaf0f
Albert Mietus <albert AT mietus DOT nl>
2023-09-19 21:48:49 +0200

    refactored
    --HG--
    branch : dev-writers

--- pytst/writers/RPy/__init__.py
+++ pytst/writers/RPy/__init__.py
@@ -1,4 +1,15 @@
 # (C) Albert Mietus, 2023. Part of Castle/CCastle project  -- PYTEST init for RPY
 
 
+def get_dirPath_of_file(f=__file__): 
+    from pathlib import Path
+    import os
+    print("XXXX", Path(os.path.realpath(f)))
+    return Path(os.path.realpath(f)).parent
+
+def end_with_NL(txt):
+    return txt +'\n' if (txt[-1] != '\n') else txt
+
+
+
 
--- pytst/writers/RPy/test_0.py
+++ pytst/writers/RPy/test_0.py
@@ -10,25 +10,20 @@ from castle.writers import RPy
 
 from pathlib import Path
 
-def test_run():
-    print(f"{__name__}::")
+#OLD
+#def test_run():
+#    print(f"{__name__}::")
 
-def test_import():
-    print(f"{__name__}:: {RPy._version}")
+#OLD
+#def test_import():
+#    print(f"{__name__}:: {RPy._version}")
 
 
-def _get_file_dirPath():
-    from pathlib import Path
-    import os
-    return Path(os.path.realpath(__file__)).parent
-
-def end_with_NL(txt):
-    return txt +'\n' if (txt[-1] != '\n') else txt
 
 template_dir ="./templates/"
 def test_load_template():
     HW_file = 'HW_template.txt'
-    t_dir = _get_file_dirPath() / template_dir
+    t_dir = get_dirPath_of_file(__file__) / template_dir
 
     template = RPy.Template(searchpath=t_dir, template=HW_file)
     out=template.render(Hello='{{Hello}}', World='{{World}}')           # Generates itself
