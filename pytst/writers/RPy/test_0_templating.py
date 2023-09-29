38f2ee507e605fb85932da96e625fd07a84dca09
Albert Mietus <albert AT mietus DOT nl>
2023-09-29 11:24:40 +0200

    small refactoring
    --HG--
    branch : dev-writers

--- pytst/writers/RPy/test_0_templating.py
+++ pytst/writers/RPy/test_0_templating.py
@@ -8,16 +8,24 @@ import pytest
 from . import *
 from castle.writers import RPy
 
-template_dir ="./templates/"
-t_dir = get_dirPath_of_file(__file__) / template_dir
+my_dir =  get_dirPath_of_file(__file__) 
+template_r_dir = "./templates/"
+template_a_dir = my_dir / template_r_dir
 
 def test_simpel_template():
     HW_file = 'HW_template.txt'
-
-    template = RPy.Template(template=HW_file, search_path=t_dir)
+    template = RPy.Template(template=HW_file, search_path=template_a_dir)
     out=template.render(Hello='{{Hello}}', World='{{World}}')           # Generates itself
 
-    ref=open(t_dir / HW_file).read()
+    ref=open(template_a_dir / HW_file).read()
+    assert end_with_NL(ref) == end_with_NL(out), f"ref::\n{ref}\nout::\n{out}\nDo not match"
+
+def test_simpel_template_2():
+    HW_file = 'HW_template.txt'
+    template = RPy.Template(template=HW_file, top_dir=my_dir)
+    out=template.render(Hello='{{Hello}}', World='{{World}}')           # Generates itself
+
+    ref=open(template_a_dir / HW_file).read()
     assert end_with_NL(ref) == end_with_NL(out), f"ref::\n{ref}\nout::\n{out}\nDo not match"
 
 
@@ -25,10 +33,10 @@ def test_template_extends():
     child = 'child.txt'
     base  = 'base.txt' # Not used to render, only to test
 
-    template = RPy.Template(template=child, search_path=t_dir)
+    template = RPy.Template(template=child, search_path=template_a_dir)
     out = template.render(v1 ='Var 1', v2 ='Var 2', TOP='top: but not completely', BOTTOM="bottom, really")
 
-    ref_lines = open(t_dir / base).readlines()
+    ref_lines = open(template_a_dir / base).readlines()
     out_lines = out.splitlines(keepends=True)
 
     assert ref_lines[0] == out_lines[0], "Rendering child, should return the topline of base"
@@ -44,7 +52,7 @@ def test_template_extends():
 def test_template_parts():
     big_file  = "big.txt"                                             # Uses: part-include.txt &  part-import.txt
 
-    template = RPy.Template(template=big_file, search_path=t_dir)
+    template = RPy.Template(template=big_file, search_path=template_a_dir)
     out = template.render()
 
     assert 'INCLUDED' in out, "this line in part-include.txt"
