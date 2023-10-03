d4c3e1431cf80fb752620a7956c168a629ddde42
Albert Mietus <albert AT mietus DOT nl>
2023-10-03 12:46:34 +0200

    Refactored pytst on protocol with better (in memory) generatedProtocol_verifier
    --HG--
    branch : dev-writers

--- pytst/writers/RPy/test_99_SieveMoats.py
+++ pytst/writers/RPy/test_99_SieveMoats.py
@@ -13,24 +13,23 @@ from castle.writers import RPy
 
 @pytest.fixture
 def generatedProtocol_verifier():
-    def file_matcher(aigr_dummy, td):
+     def matcher(aigr_mock, td):
         template = RPy.Template("protocol.jinja2")
-        out = template.render(protocols=(aigr_dummy,))
-        with  open(td.gen_file, 'w') as f:
-            f.write(out)
-        assert filecmp.cmp(td.gen_file, td.ref_file), f"The generated file ({td.gen_file}) and the reference ({td.ref_file}) are not the same"
-    return file_matcher
+        out = template.render(protocols=(aigr_mock,))
+        ref = open(td.ref_file).read()
+        assert out == ref
+     return matcher
 
 
 def test_01_StartSieve(generatedProtocol_verifier):
-    generatedProtocol_verifier(aigr_dummy=Sieve.StartSieve, td=TstDoubles('protocols/StartSieve'))
+    generatedProtocol_verifier(aigr_mock=Sieve.StartSieve, td=TstDoubles('protocols/StartSieve'))
 
 def test_02_SlowStart(generatedProtocol_verifier):
-    generatedProtocol_verifier(aigr_dummy=Sieve.SlowStart, td=TstDoubles('protocols/SlowStart'))
+    generatedProtocol_verifier(aigr_mock=Sieve.SlowStart, td=TstDoubles('protocols/SlowStart'))
 
 
 
 @pytest.mark.skip("After SlowStart")
-def test_03_SimpleSieve(T_Protocol):
+def test_03_SimpleSieve(generatedProtocol_verifier):
         assert False
 
