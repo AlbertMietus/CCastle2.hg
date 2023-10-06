d62c3b6e1fd881e72f052367a521e222198f5f86
Albert Mietus <albert AT mietus DOT nl>
2023-10-06 13:01:39 +0200

    SAVE_FILE is not default-on, when testing to render the SimpleMoat-protocols
    --HG--
    branch : dev-writers

--- pytst/writers/RPy/test_99_SieveMoats.py
+++ pytst/writers/RPy/test_99_SieveMoats.py
@@ -7,12 +7,14 @@ from . import TstDoubles, generatedProtocol_verifier, T_Protocol
 ##Note: T_Protocol is used in generatedProtocol_verifier; but need to be in this scope
 
 
+SAVE_FILE=True
+
 def test_01_StartSieve(generatedProtocol_verifier):
-    generatedProtocol_verifier(aigr_mock=Sieve.StartSieve, td=TstDoubles('protocols/StartSieve'))
+    generatedProtocol_verifier(aigr_mock=Sieve.StartSieve, td=TstDoubles('protocols/StartSieve'), save_file=SAVE_FILE)
 
 def test_02_SlowStart(generatedProtocol_verifier):
-    generatedProtocol_verifier(aigr_mock=Sieve.SlowStart, td=TstDoubles('protocols/SlowStart'))
+    generatedProtocol_verifier(aigr_mock=Sieve.SlowStart, td=TstDoubles('protocols/SlowStart'), save_file=SAVE_FILE)
 
 def test_03_SimpleSieve(generatedProtocol_verifier):
-    generatedProtocol_verifier(aigr_mock=Sieve.SimpleSieve, td=TstDoubles('protocols/SimpleSieve'), save_file=True)
+    generatedProtocol_verifier(aigr_mock=Sieve.SimpleSieve, td=TstDoubles('protocols/SimpleSieve'), save_file=SAVE_FILE)
 
