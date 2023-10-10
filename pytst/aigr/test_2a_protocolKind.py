9167da27fb79da516d51a9403bb4d1f7881fe2d0
Albert Mietus <albert AT mietus DOT nl>
2023-10-10 21:51:30 +0200

    (re)Added ConsecutiveSeries test for ProtocolKind, to remove surviving mutants
    --HG--
    branch : dev-writers

--- pytst/aigr/test_2a_protocolKind.py
+++ pytst/aigr/test_2a_protocolKind.py
@@ -14,3 +14,12 @@ def test_1_NameExist():
     assert ProtocolKind.Event
     assert ProtocolKind.Data
     assert ProtocolKind.Stream
+
+def test_2_ConsecutiveSeries():
+    values = sorted(k.value for k in ProtocolKind)
+    v1 = values[0]
+    for v2 in values[1:]:
+      assert v1+1==v2, f"{ProtocolKind(v1)}+1 <> {ProtocolKind(v2)}"
+      v1=v2
+    
+    
