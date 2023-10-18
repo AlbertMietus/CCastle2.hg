f5a82fbcdeee48be64438a2986ed9af1ce275a15
Albert Mietus <albert AT mietus DOT nl>
2023-10-18 16:06:13 +0200

    ToDo: 'ID' class -- make a dummy test with docs for it
    --HG--
    branch : dev-writers

--- pytst/aigr/test_0_AIGR.py
+++ pytst/aigr/test_0_AIGR.py
@@ -1,5 +1,7 @@
 # (C) Albert Mietus, 2023. Part of Castle/CCastle project
 
+import pytest
+
 from castle.aigr import AIGR
 
 class Demo(AIGR):
@@ -15,3 +17,13 @@ def test_noAIGR():
 def test_AIGR_sub():
     d = Demo()
     assert isinstance(d, AIGR)
+
+@pytest.mark.skip("The ID comes A bit later")
+def test_ID_many():
+    assert False, """Make an ID class in AIGR (or generic). ((also see NamedNode))
+
+    It holds the name in AIGR -nodes ipv de str class. Model it a bit like pathlib.Path
+
+    We should have atomic_ID, relative_ID, DottedName_ID, ect
+    """ 
+
