9645523fe8dede190d483eeced7c40836f9eac66
Albert Mietus <albert AT mietus DOT nl>
2024-03-01 17:03:34 +0100

    use 'EXTEDED_PYPATH' for pytest and friend in make <testing>
    --HG--
    branch : dev-writers

--- Mk/testing.mk
+++ Mk/testing.mk
@@ -1,22 +1,23 @@
-PYTEST=PYTHONPATH=`pwd` pytest
+EXTEDED_PYPATH=PYTHONPATH=`pwd`
+PYTEST=${EXTEDED_PYPATH} pytest
 PYTEST_OPTONS=-rxXsfE
 
 test coverage:
-	coverage run --branch -m pytest ${PYTEST_OPTONS}  pytst/
+	${EXTEDED_PYPATH} coverage run --branch -m pytest ${PYTEST_OPTONS}  pytst/
 	coverage report  --skip-covered
 	coverage html --directory=${COVERAGE_dir}
 coverage-open: coverage
 	open ${COVERAGE_dir}index.html
 
 pytest pytest-only:							# No coverage reports
-	pytest ${PYTEST_OPTONS}  pytst
+	${PYTEST} ${PYTEST_OPTONS}  pytst
 pytest-s:								# -s : No capure (so, show stdout/stderr)
-	pytest ${PYTEST_OPTONS} -s pytst
+	${PYTEST} ${PYTEST_OPTONS} -s pytst
 pytest-d pytest-ds pytest-sd:						# with debuging
-	pytest ${PYTEST_OPTONS} --log-cli-level=DEBUG -s pytst/
+	${PYTEST} ${PYTEST_OPTONS} --log-cli-level=DEBUG -s pytst/
 
 mutmut: # Mutation testing (takes a long run) https://en.wikipedia.org/wiki/Mutation_testing
-	-mutmut run  --paths-to-exclude ARCHIVED --tests-dir pytst --paths-to-mutate castle  --runner "pytest -x   pytst/"
+	${EXTEDED_PYPATH} mutmut run  --tests-dir pytst --paths-to-mutate castle  --runner "pytest -x   pytst/"
 	mutmut html && rm -rf ${MUTMUT_dir} && mv html ${MUTMUT_dir}
 	mutmut results
 mutmut-open: mutmut
