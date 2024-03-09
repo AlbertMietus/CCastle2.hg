cebb104d0d0b351670568198773fb656fe4f841d
Albert Mietus <albert AT mietus DOT nl>
2024-03-09 17:09:51 +0100

    Make pyanalyse/pyreverse: no color, as it hides the lines in a class
    --HG--
    branch : dev-writers

--- Mk/pyanalyse.mk
+++ Mk/pyanalyse.mk
@@ -9,8 +9,8 @@ ${PYREVERSE_dir}:; mkdir $@
 pyanalyse pyreverse: ${PYREVERSE_dir}
 	for P in ${PYREVERSE_PKGS}; do \
 		echo "PYANALYSE::" $$P "...";\
-		pyreverse -d ${PYREVERSE_dir} -o ${PYREVERSE_FORMAT} ${PYREVERSE_OPTIONS} -p $$P-noModules -mn --colorized --max-color-depth=42  $$P ;\
-		pyreverse -d ${PYREVERSE_dir} -o ${PYREVERSE_FORMAT} ${PYREVERSE_OPTIONS} -p $$P-Modules   -my --colorized --max-color-depth=42  $$P ;\
+		pyreverse -d ${PYREVERSE_dir} -o ${PYREVERSE_FORMAT} ${PYREVERSE_OPTIONS} -p $$P-noModules -mn --max-color-depth=42  $$P ;\
+		pyreverse -d ${PYREVERSE_dir} -o ${PYREVERSE_FORMAT} ${PYREVERSE_OPTIONS} -p $$P-Modules   -my --max-color-depth=42  $$P ;\
 	done
 	@echo ".. done. Result; see: ./${PYREVERSE_dir}"
 
