e4011fbd46320772e7374c3594b6a6691c9bc73f
Albert Mietus <albert AT mietus DOT nl>
2024-03-10 22:41:02 +0100

    make install-e/fast: use workaround for mypy + Editable installs
    --HG--
    branch : dev-writers

--- Mk/build.mk
+++ Mk/build.mk
@@ -1,6 +1,16 @@
 build: all
 	python -m build
 
-install-e: build
-	pip install -e .
+install-e: build _install
+install-fast: _install
+
+#_install: _install-does-not-work
+_install: _install-workaround
+
+_install-does-not-work:
+	pip install -e .  # does not work for mypy, use workaround
+_install-workaround: 
+	echo "from setuptools import setup; setup()" >setup.py
+	SETUPTOOLS_ENABLE_FEATURES="legacy-editable" pip install -e .
+	rm setup.py
 
