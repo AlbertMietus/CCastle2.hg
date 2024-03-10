3f084f5c1a3a8b646cdd5e6d74dea5ba4a09c150
Albert Mietus <albert AT mietus DOT nl>
2024-03-10 21:54:40 +0100

    Add type:'Specialise' to Protocol.based_on, for mypy
    --HG--
    branch : dev-writers

--- base_packages/castle-aigr/castle/aigr/protocols.py
+++ base_packages/castle-aigr/castle/aigr/protocols.py
@@ -44,7 +44,7 @@ class Protocol(NamedNode):
 
     _: KW_ONLY
     kind             :ProtocolKind
-    based_on         :PTH.Optional[Protocol]=dc_field(default_factory= lambda :Protocol._BASE)      # pragma: no mutate
+    based_on         :PTH.Optional[Protocol|Specialise]=dc_field(default_factory= lambda :Protocol._BASE)      # pragma: no mutate
     typedParameters  :PTH.Optional[PTH.Sequence[TypedParameter]]=()
 
 
