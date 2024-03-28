fd6a9afa76823a4e3b6e5404e060799782533b06
Albert Mietus <albert AT mietus DOT nl>
2024-03-28 14:05:56 +0100

    Added TESTDOUBLES::ComponentInterface(s) for (basic) sieve
    --HG--
    branch : dev-writers

--- TestDoubles_packages/TestDoubles-aigr-sieve/pytst/__init__.py
+++ TestDoubles_packages/TestDoubles-aigr-sieve/pytst/__init__.py
@@ -12,17 +12,17 @@ def verify_Protocol(p, name, my_event_names, total_no_of_event=None, base=None, 
         cls = aigr.EventProtocol
 
     assert isinstance(p, cls)
-    assert p.name == name,  f"{p.name} reported but expected: {name}"
+    assert str(p.name) == name,  f"{p.name} reported but expected: {name}"
     assert p.based_on is base
     assert p._noEvents() == no_events, f"{p.name} reports {p._noEvents()} events, but expected: {no_events} event(s)"
     for no, name in enumerate(my_event_names):
-        assert p.events[no].name == name, f"{p.name} (own/local) event no={no}: {p.events[no].name}, expected: {name}"
+        assert str(p.events[no].name) == name, f"{p.name} (own/local) event no={no}: {p.events[no].name}, expected: {name}"
 
 
 
 def verify_NS(ns, name, registered_names, as_name=None):
     if as_name is None: as_name=name
-    assert ns.name == as_name, f"verify_NS:: name={ns.name}, expected: {as_name}\n\tns={ns}"
+    assert str(ns.name) == as_name, f"verify_NS:: name={ns.name}, expected: {as_name}\n\tns={ns}"
     for n in registered_names:
         if isinstance(n, (list, tuple)):
             # This is hardly/not used: but .... We support `import n[1] as n[0]`
@@ -30,4 +30,17 @@ def verify_NS(ns, name, registered_names, as_name=None):
             name, asName = n[1], n[0]
         else:
             name, asName = n, n
-        assert ns.getID(asName).name == name
+        assert str(ns.getID(asName).name) == name
+
+def verify_ComponentInterface(i, name, my_port_names=[], total_no_of_ports=None):
+    if total_no_of_ports is None:
+        total_no_of_ports = len(my_port_names)
+
+    assert isinstance(i, aigr.ComponentInterface)
+    assert str(i.name) == name, f"{i.name} reported but expected: {name}"
+
+    assert i._noPorts() == total_no_of_ports, f"{i.name}._noPort()->{i._noPorts()}, expected {total_no_of_ports}"
+    for no, name in enumerate(my_port_names):
+        assert str(i.ports[no].name) == name, f"{i.name} (own/local) port no={no}: {i.ports[no].name}, expected: {name} (str-compare)"
+        assert i.ports[no].name == aigr.ID(name),  f"{i.name} (own/local) port no={no}: {i.ports[no].name}, expected: {name} (ID)"
+
