abcfc0c01508e738bd33452fbc76565d7d410025
Albert Mietus <albert AT mietus DOT nl>
2024-04-22 16:10:29 +0200

    AIGR: Already added some more operators; ToDo: expressions/builders/tests

--- base_packages/castle-aigr/castle/aigr/expressions/operators.py
+++ base_packages/castle-aigr/castle/aigr/expressions/operators.py
@@ -10,11 +10,40 @@ from dataclasses import field as dc_field
 from .. import AIGR
 
 class _operator(AIGR): pass
-class _bin_op(_operator): pass
 
-class Power(_bin_op)  :'**'
-class Times(_bin_op)  : '*'
-class Div(_bin_op)    : '/'
-class Modulo(_bin_op) : '%'
-class Add(_bin_op)    : '+'
-class Sub(_bin_op)    : '-'
+class _bin_op(_operator):    "Operate on two values"                      # Why not cascaded?
+class Power(_bin_op)   		:'**'
+class Times(_bin_op)        : '*'
+class Div(_bin_op)          : '/'
+class Modulo(_bin_op)       : '%'
+class Add(_bin_op)          : '+'
+class Sub(_bin_op)          : '-'
+#class MatrixMult(_bin_op)	: '@'    	# Probably NOT needed
+
+class _compare_op(_operator): "Compare 2 or more values --The expression can be cascaded `1<2<3`"
+class Equal(_compare_op):         '=='
+class Notequal(_compare_op):      '!='
+#class Uequal(_compare_op):        '<>'   	# Probably NOT needed
+class Less(_compare_op):          '<'
+class LessEqual(_compare_op):     '<='
+class Greater(_compare_op):       '>'
+class GreaterEqual(_compare_op):  '>='
+class Is(_compare_op):            'is'		# Probably needed
+class IsNot(_compare_op):         'is not'	# Probably needed
+
+
+class _boolean_op(_operator): "Operate on boolean values"
+class And(_boolean_op):  	'and'
+class Nand(_boolean_op):  	'nand'
+class Or(_boolean_op):  	'or'
+class Nor(_boolean_op):  	'nor'
+class Xor(_boolean_op):  	'xor'
+class Xnor(_boolean_op):  	'xnor'
+
+
+class _unart_op(_operator): "Operates on only one value"
+class Not(_unart_op):  			'not'            #Unary not
+class Negative(_unart_op): 		'- (unary)'
+class Positive(_unart_op): 		'+ (unary)'
+
+
