2cc41d0d9a3a0db6f05236b2e6f442c279ba22cd
Albert Mietus <albert AT mietus DOT nl>
2024-04-28 17:49:54 +0200

    refactored aigr.(meta)builder

--- base_packages/castle-aigr/castle/aigr/builders/build_expr.py
+++ base_packages/castle-aigr/castle/aigr/builders/build_expr.py
@@ -26,39 +26,24 @@ if False: # This code-block is passive; it's ref/doc only
 # * The name of the builder is the same as the name of the operators.<cls>
 # * We can add names:functions to this module, by writing to `globals()`
 #    ``globals()[op_name] = lambda .....
-def _meta_LR():
-    """Meta-build all builders for all LRexpression; which all use the _LeftAssociative operator
 
-    See the ref/code (in the module-source) above, how each function looks like.
 
-    This function is called directly below, in this file -- having the details in function-scope 
-    makes it a bit more readable, without cluttering the module-scope."""
+def _meta_build(super_op, expr):
+    """Meta-build the builders for (the subclasses of super_op"
 
-    def _build(op):
-        return lambda left,right, *more : LRexpression(op=op(), values=(left,right)+more)
+    See the ref/code (in the module-source) above, how each function looks like."""
 
-    pubs    = [n for n in dir(operators) if n[0]!='_']                        # The 'public' names in 'operators'
-    clsses  = [c for n in pubs   if isinstance(c:=getattr(operators,n),type)] # all classes
-    ops     = [c for c in clsses if issubclass(c, operators._LeftAssociative)] # all operator (classes) that are LeftAssociative
-    for op in ops:
-        op_name = op.__name__
-        logger.debug(f"Creating builder '{op_name}' around {op}")
-        globals()[op_name] = _build(op)
-
-def _meta_RL():
-    """Meta-build the RLexpression builders, like Power"""
-
-    def _build(op):
-        return lambda left,right, *more : RLexpression(op=op(), values=(left,right)+more)
+    def _build(op, association):
+        return lambda left,right, *more : association(op=op(), values=(left,right)+more)
 
     pubs    = [n for n in dir(operators) if n[0]!='_']                        # The 'public' names in 'operators'
     clsses  = [c for n in pubs   if isinstance(c:=getattr(operators,n),type)] # all classes
-    ops     = [c for c in clsses if issubclass(c, operators._RightAssociative)] # all operator (classes) that are LeftAssociative
+    ops     = [c for c in clsses if issubclass(c, super_op)] # all operator (classes) that are Left/Right-Associative
     for op in ops:
         op_name = op.__name__
         logger.debug(f"Creating builder '{op_name}' around {op}")
-        globals()[op_name] = _build(op)
+        globals()[op_name] = _build(op, expr)
 
-#Call the meta-builders
-_meta_LR()
-_meta_RL()
+#Call the meta-builder
+_meta_build(super_op=operators._LeftAssociative,  expr=LRexpression)
+_meta_build(super_op=operators._RightAssociative, expr=RLexpression)
