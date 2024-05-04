fdbcef5590869ddd872195be36da50fb8f96586e
Albert Mietus <albert AT mietus DOT nl>
2024-05-04 16:15:51 +0200

    RPy witer can't be tested, before AIGR/TESTDOUBLES are updated (skip)

--- core_packages/castle-RPy-writer/pytst/test_3_SieveProtocols.py
+++ core_packages/castle-RPy-writer/pytst/test_3_SieveProtocols.py
@@ -2,6 +2,9 @@
 """Verify the SieveProtocols are correctly 'generated' with the RPy backend.
 
    - It used/reads a (manually crafted) AIGR, see  ``.../TestDoubles/AIGR/``,
+   - the variant "basic1" is used -- so:
+     * no Generics
+     * no SlowStart
    - the output is rendered, using a (jinja) Template (T_Protocol)
    - and compared to a reference-file ``.../TestDoubles/reference/`` - same of not
    - optionally, the output is aslo written to file (``.../TestDoubles/_generated/``),
@@ -9,23 +12,19 @@
 """
 
 import pytest
-from castle.TESTDOUBLES.aigr import sieve
+from castle.TESTDOUBLES.aigr.sieve.basic1 import protocols
 
 from . import TstDoubles, generatedProtocol_verifier, T_Protocol
 ##Note: T_* are used in **_verifier; but need to be in this scope (or pytest can't find it)
 
 
-def test_01_StartSieve(generatedProtocol_verifier):
-    generatedProtocol_verifier(aigr_mocks=sieve.StartSieve, td=TstDoubles('protocols/start_sieve'), strip_remarker=True)
 
-def test_02_SlowStart(generatedProtocol_verifier):
-    generatedProtocol_verifier(aigr_mocks=sieve.SlowStart, td=TstDoubles('protocols/slow_start'), strip_remarker=True)
 
-@pytest.mark.xfail(reason="Need to design (the generated code) for Specialise/Generic Protocols")
-def test_03_SimpleSieve_withGeneric(generatedProtocol_verifier):
-    """``SimpleSieve`` depend on the instantiated generic ``SlowStart_1``.
-        They are in the same namespace and (so) need to be rendered in the same file.
-        And so, tested together!"""
-    generatedProtocol_verifier(aigr_mocks=(sieve.SlowStart_1, sieve.SimpleSieve),
-                                   td=TstDoubles('protocols/simple_sieve'), strip_remarker=True)
+@pytest.mark.skip(reason="no way of currently testing this")
+def test_StartSieve(generatedProtocol_verifier):
+    generatedProtocol_verifier(aigr_mocks=protocols.StartSieve, td=TstDoubles('protocols/start_sieve'), strip_remarker=True)
+
+@pytest.mark.skip(reason="no way of currently testing this")
+def test_SimpleSieve(generatedProtocol_verifier):
+    generatedProtocol_verifier(aigr_mocks=protocols.StartSieve, td=TstDoubles('protocols/simple_sieve'), strip_remarker=True)
 
