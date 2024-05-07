4e6bf43749ce33ca8c864bae30f4e83676661979
Albert Mietus <albert AT mietus DOT nl>
2024-05-07 21:36:51 +0200

    cleanup

--- base_packages/castle-aigr/castle/aigr/expressions/operators.py
+++ base_packages/castle-aigr/castle/aigr/expressions/operators.py
@@ -1,11 +1,6 @@
 # (C) Albert Mietus, 2023-2024. Part of Castle/CCastle project
 
 from __future__ import annotations # Postponed evaluation of annotations
-import logging; logger = logging.getLogger(__name__)
-
-import typing as PTH                                                                                 # Python TypeHints
-from dataclasses import dataclass, KW_ONLY
-from dataclasses import field as dc_field
 
 from .. import AIGR
 
