e8e4fd3dd256ce5cc1f5d2ae239e24d44be9c05e
Albert Mietus <albert AT mietus DOT nl>
2024-05-09 14:41:38 +0200

    make pyanalyse concurrent

--- Mk/pyanalyse.mk
+++ Mk/pyanalyse.mk
@@ -9,9 +9,10 @@ ${PYREVERSE_dir}:; mkdir $@
 pyanalyse pyreverse: ${PYREVERSE_dir}
 	for P in ${PYREVERSE_PKGS}; do \
 		echo "PYANALYSE::" $$P "...";\
-		pyreverse -d ${PYREVERSE_dir} -o ${PYREVERSE_FORMAT} ${PYREVERSE_OPTIONS} -p $$P-noModules -mn --max-color-depth=42  $$P ;\
-		pyreverse -d ${PYREVERSE_dir} -o ${PYREVERSE_FORMAT} ${PYREVERSE_OPTIONS} -p $$P-Modules   -my --max-color-depth=42  $$P ;\
+		pyreverse -d ${PYREVERSE_dir} -o ${PYREVERSE_FORMAT} ${PYREVERSE_OPTIONS} -p $$P-noModules -mn --max-color-depth=42  $$P & \
+		pyreverse -d ${PYREVERSE_dir} -o ${PYREVERSE_FORMAT} ${PYREVERSE_OPTIONS} -p $$P-Modules   -my --max-color-depth=42  $$P & \
 	done
+	wait
 	if [ "plantuml" = ${PYREVERSE_FORMAT} ] ;then (\
 		echo "PYANALYSE: plantUML processing (all)"; \
 		cd  ${PYREVERSE_dir}; plantuml -tsvg ./*.plantuml); fi
