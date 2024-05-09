4e1c9f39df74084535fc039bb7ac49cdae046ee6
Albert Mietus <albert AT mietus DOT nl>
2024-05-09 13:47:29 +0200

    tivial cleanup

--- Mk/build.mk
+++ Mk/build.mk
@@ -9,7 +9,7 @@ _install: _install-workaround
 
 _install-does-not-work:
 	pip install -e .  # does not work for mypy, use workaround
-_install-workaround: 
+_install-workaround:
 	echo "from setuptools import setup; setup()" >setup.py
 	SETUPTOOLS_ENABLE_FEATURES="legacy-editable" pip install -e .
 	rm setup.py
