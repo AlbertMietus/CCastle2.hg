7c37a7573a6ad90159d9b9661a7dbb3db465a0b4
Albert Mietus <albert AT mietus DOT nl>
2024-05-29 16:09:35 +0200

    Refactor::MutMut use triple-str for docstr; to prevent mutations

--- base_packages/castle-aigr/castle/aigr/expressions/operators.py
+++ base_packages/castle-aigr/castle/aigr/expressions/operators.py
@@ -6,45 +6,45 @@ from .. import AIGR
 
 class _operator(AIGR): pass
 
-class _LeftAssociative(_operator)    : "Left associative operators"
-class Times(_LeftAssociative)        : '*'
-class Div(_LeftAssociative)          : '/'
-class Modulo(_LeftAssociative)       : '%'
-class Add(_LeftAssociative)          : '+'
-class Sub(_LeftAssociative)          : '-'
-#class MatrixMult(_LeftAssociative)	 : '@'    	# Probably NOT needed
+class _LeftAssociative(_operator)    : """Left associative operators"""
+class Times(_LeftAssociative)        : '''*'''
+class Div(_LeftAssociative)          : '''/'''
+class Modulo(_LeftAssociative)       : '''%'''
+class Add(_LeftAssociative)          : '''+'''
+class Sub(_LeftAssociative)          : '''-'''
+#class MatrixMult(_LeftAssociative)	 : '''@'''    	# Probably NOT needed
 
-class _RightAssociative(_operator)   : "Right associative operators"
-class Power(_RightAssociative)       : '**'
+class _RightAssociative(_operator)   : """Right associative operators"""
+class Power(_RightAssociative)       : '''**'''
 
-class _compare_op(_operator)         : "Compare 2 or more values --The expression can be cascaded `1<2<3`, always resulting in True/False"
-class Equal(_compare_op)             : '=='
-class NotEqual(_compare_op)          : '!='
-#class Unequal(_compare_op)           : '<>'   	# Probably NOT needed
-class Less(_compare_op)              : '<'
-class LessEqual(_compare_op)         : '<='
-class Greater(_compare_op)           : '>'
-class GreaterEqual(_compare_op)      : '>='
-class Is(_compare_op)                : 'is'
-class IsNot(_compare_op)             : 'is not'
-class In(_compare_op)                : 'in'
-class NotIn(_compare_op)             : 'not in'
+class _compare_op(_operator)         : """Compare 2 or more values --The expression can be cascaded `1<2<3`, always resulting in True/False"""
+class Equal(_compare_op)             : '''=='''
+class NotEqual(_compare_op)          : '''!='''
+#class Unequal(_compare_op)           : '''<>'''   	# Probably NOT needed
+class Less(_compare_op)              : '''<'''
+class LessEqual(_compare_op)         : '''<='''
+class Greater(_compare_op)           : '''>'''
+class GreaterEqual(_compare_op)      : '''>='''
+class Is(_compare_op)                : '''is'''
+class IsNot(_compare_op)             : '''is not'''
+class In(_compare_op)                : '''in'''
+class NotIn(_compare_op)             : '''not in'''
 
-class _boolean_op(_operator)         : "Operate on boolean values"
-class _ShortCircuit(_operator)       : "Operators, that stop when the result is fixed -- resulting in the 'last' value"
+class _boolean_op(_operator)         : """Operate on boolean values"""
+class _ShortCircuit(_operator)       : """Operators, that stop when the result is fixed -- resulting in the 'last' value"""
 class _bool_SC_op(_boolean_op, _ShortCircuit): pass
-class And(_bool_SC_op)  	         : 'and'
-class Nand(_bool_SC_op)  	         : 'nand'
-class Or(_bool_SC_op)  	             : 'or'
-class Nor(_bool_SC_op)  	         : 'nor'
+class And(_bool_SC_op)  	         : '''and'''
+class Nand(_bool_SC_op)  	         : '''nand'''
+class Or(_bool_SC_op)  	             : '''or'''
+class Nor(_bool_SC_op)  	         : '''nor'''
 #XOR/XNOR can not Short Circuit
-class Xor(_boolean_op)  	         : 'xor'
-class Xnor(_boolean_op)  	         : 'xnor'
+class Xor(_boolean_op)  	         : '''xor'''
+class Xnor(_boolean_op)  	         : '''xnor'''
 
 
-class _unart_op(_operator): "Operates on only one value"
-class Not(_unart_op, _boolean_op)    : 'not'            #Unary not -- always True/False
-class Negative(_unart_op) 		     : '- (unary)'
-class Positive(_unart_op) 		     : '+ (unary)'
+class _unart_op(_operator)           : """Operates on only one value"""
+class Not(_unart_op, _boolean_op)    : '''not'''            #Unary not -- always True/False
+class Negative(_unart_op) 		     : '''- (unary)'''
+class Positive(_unart_op) 		     : '''+ (unary)'''
 
 
