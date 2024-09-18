f92de8870695f2e99a6d0a57a82175647438a995
Albert Mietus <albert AT mietus DOT nl>
2024-09-18 12:28:20 +0200

    Update a test on (Source) NameSpaces: source file is expected to be related to name of the namenspace (but no fail) --also solving a bug of last week

--- TestDoubles_packages/TestDoubles-aigr-sieve/pytst/sieve/basic1/test_2_sieve_NS.py
+++ TestDoubles_packages/TestDoubles-aigr-sieve/pytst/sieve/basic1/test_2_sieve_NS.py
@@ -27,7 +27,12 @@ def test_0a_all_sieveProtocols_exist():
 def test_0b_all_NS_have_source():
     for ns in (getattr(sieve_namespaces, name) for name in dir(sieve_namespaces)
                    if isinstance(getattr(sieve_namespaces, name), aigr.Source_NS)):
-        assert ns.source == ns.name +'.Moat'
+        assert ns.source is not None, f"No source(file) is set for namespace:'{ns.name}'"
+        # The two test below will not fail, but give a warning
+        if not ns.name in ns.source:
+            pytest.skip(f"Warning: namespace-name: '{ns.name}' is not in filename: `{ns.source}`")
+        if not ns.source.split('.')[-1].capitalize() in ('Castle', 'Moat'):
+            pytest.skip(f"Warning: namespace-source: '{ns.source}' is not using a standard extension (.Castle, .Moat)")
 
 
 
