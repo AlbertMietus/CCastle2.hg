635caa641ae745fc198ebc9c8d33768f512598e5
Albert Mietus <albert AT mietus DOT nl>
2024-12-14 14:35:05 +0100

    Make XXX ToDo Really & show, to show annotations in code

--- Mk/dirs.mk
+++ Mk/dirs.mk
@@ -6,4 +6,9 @@ include ${TOPd}Mk/recursive.mk
 find_Makefiles:
 	find . -type f -iname Makefile | fgrep -v ./Makefile | sort
 
+XXX ToDo Really:
+	-! grep $@ `find ${SUBS} -type f -iname \*.py` /dev/null # Reverse and ignore error-code
+
+show: XXX ToDo Really
+
 
