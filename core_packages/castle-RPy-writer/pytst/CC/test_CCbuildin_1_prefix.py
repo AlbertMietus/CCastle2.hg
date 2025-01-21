5fe787148430eb5938ecb4510caa23431fec4f22
Albert Mietus <albert AT mietus DOT nl>
2025-01-21 21:31:24 +0100

    RPy-writer CC/buildin prefix test refactored, added docs

--- core_packages/castle-RPy-writer/pytst/CC/test_CCbuildin_1_prefix.py
+++ core_packages/castle-RPy-writer/pytst/CC/test_CCbuildin_1_prefix.py
@@ -5,13 +5,17 @@ import pytest
 from castle.writers.RPy.CC import buildin
 
 def verifyClassPrefix(cls, prefix):
+    """Verify the prefix/name/existance of some buildin classes.
+
+    Those names are hardcoded in CC/buildin/, which is inported in the generated .rpy files.
+    At the same time, those names are used when generating the rpy files,
+    And so, those should match (or the rpy file(s) will not compile/run"""
+
     assert isinstance(cls, type), 			f'{cls} is not a class'
     assert cls.__name__.startswith(prefix), f'Incorrect prefix: Expected "{prefix}", found "{cls.__name__}" for for {cls}'
 
 def test_prefixes_1_elementalHW():
-    """test the prefix/name/existance of some buildin classes, ..
-    Those used by elemental HelloWorld (TestDoubles/HelloWorlds/elemental/HelloWorld.rpy)
-    """
+    """Verify those used by elemental HelloWorld (TestDoubles/HelloWorlds/elemental/HelloWorld.rpy) """
     verifyClassPrefix(buildin.CC_B_ComponentInterface,	'CC_B_')
     verifyClassPrefix(buildin.CC_B_Component, 			'CC_B_')
     verifyClassPrefix(buildin.CC_B_ComponentClass, 		'CC_B_')
@@ -20,6 +24,7 @@ def test_prefixes_1_elementalHW():
 
 
 def test_prefixes_99_someMore():
+    """Verify some more - pragmatic selected -- move to better test later"""
     verifyClassPrefix(buildin.CC_B_C_PortID,	'CC_B_C_')
     verifyClassPrefix(buildin.CC_B_OutPort,		'CC_B_')
     verifyClassPrefix(buildin.CC_B_P_EventID,	'CC_B_P_')
