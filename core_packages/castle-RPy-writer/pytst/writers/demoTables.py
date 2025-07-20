d764da597fe2197a623fa950ec1455bd5fa21a79
Albert Mietus <albert AT mietus DOT nl>
2025-07-20 10:49:13 +0200

    ASIS/cleanup

---
+++ core_packages/castle-RPy-writer/pytst/writers/demoTables.py
@@ -0,0 +1,44 @@
+# (C) Albert Mietus, 2025. Part of Castle/CCastle project
+
+import logging; logger = logging.getLogger(__name__)
+import pytest
+
+from castle import aigr
+from castle.aigr import ID
+from castle.aigr.components import EventDispatchTable
+
+from castle.aigr_extra.blend import mangle_event_handler
+
+from .mocks import *
+
+@pytest.fixture
+def simpleTable(mockPort, stubProtocol) -> aigr.EventDispatchTable:
+    """A simple EventDispatchTable, with no inherited details."""
+
+    map = {event.name: ID(mangle_event_handler(event=event.name, port=mockPort.name, protocol=stubProtocol.name)) for event in stubProtocol.events}
+    table = EventDispatchTable(port=mockPort.name, map=map)
+
+    return table
+
+@pytest.fixture
+def childTable(mockPort, mockProtocol) -> aigr.EventDispatchTable:
+    """An EventDispatchTable, with linked to `simpleTable`"""
+
+    map = {event.name: ID(mangle_event_handler(event=event.name, port=mockPort.name, protocol=mockProtocol.name)) for event in mockProtocol.events}
+    table = EventDispatchTable(port=mockPort.name, map=map,) # XXX ToDo Add "parent"
+
+    return table
+
+Expected_4_simpleTable="""\
+cc_S_MockComp_MockPort = buildin.machinery.ChainedDict(map={
+    MockEvent_1 : an_other__name,
+    },
+    parent=None)
+"""
+
+Expected_4_childTable="""\
+cc_S_MockSuper_MockPort = buildin.machinery.ChainedDict(map={
+    MockEvent_1 : ...
+    },
+    parent=cc_S_MockComp_MockPort)
+"""
