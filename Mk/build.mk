fb4c3baa5422a4c86ee22b5a7f2b253161e33f3b
Albert Mietus <albert AT mietus DOT nl>
2025-08-19 23:03:11 +0200

    Some changes for make install (editable) with new VENV (Python3.13) -- with a bit of help of chatGTP

--- Mk/build.mk
+++ Mk/build.mk
@@ -1,18 +1,11 @@
 # (C) Albert Mietus, 2025. Part of Castle/CCastle project
 
-build: all
+build: fast
 	python -m build
 
-install-e: build _install
-install-fast: _install
+install: build
+	pip install -e .
 
-#_install: _install-does-not-work
-_install: _install-workaround
-
-_install-does-not-work:
-	pip install -e .  # does not work for mypy, use workaround
-_install-workaround:
-	echo "from setuptools import setup; setup()" >setup.py
-	SETUPTOOLS_ENABLE_FEATURES="legacy-editable" pip install -e .
-	rm setup.py
+installed:
+	pip list| grep castle | sort
 
