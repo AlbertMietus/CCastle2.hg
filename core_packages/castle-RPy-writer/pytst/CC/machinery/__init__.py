a292d5879336c8933137321f9310f7601b3abee8
Albert Mietus <albert AT mietus DOT nl>
2025-08-31 10:52:07 +0200

    starting with testing the machinery-writer. Also added a note about the other (library) part

---
+++ core_packages/castle-RPy-writer/pytst/CC/machinery/__init__.py
@@ -0,0 +1,6 @@
+# (C) ALbert Mietus, 2025. Part of Castle/CCastle project
+
+"""pytst/CC/machine/ tests the library part of the machine -- so the RPython code that will be "copied" into the executable.
+
+   See pytst/machine/ for the test that of writer-part"""
+
---
+++ core_packages/castle-RPy-writer/pytst/machinery/__init__.py
@@ -0,0 +1,5 @@
+# (C) ALbert Mietus, 2025. Part of Castle/CCastle project
+
+"""pytst/machine/ tests the machinery part of writer. -- not the library part.
+
+  See pytst/CC/machine/ for those tests
