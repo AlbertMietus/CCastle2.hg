83b7cf0468ac2ce9d6deb33715381325975f490d
Albert Mietus <albert AT mietus DOT nl>
2025-10-05 22:54:44 +0200

    fixed mypy 'Cannot find implementation or library stub for module ...'

--- Mk/mypy.mk
+++ Mk/mypy.mk
@@ -13,7 +13,7 @@ mypy: mypy_castle
 
 mypy_castle:
 	@echo "MYPY::"
-	mypy	--follow-imports=skip --exclude=/_ref/ 	castle
-	mypy	                      --exclude=/_ref/ 	pytst
+	mypy  --follow-imports=skip --exclude=/_ref/    castle
+	mypy                       --exclude=/_ref/     pytst
 
 
