961390cb2faf11f11536e8a70fc5352c343c9b32
Albert Mietus <albert AT mietus DOT nl>
2025-10-13 21:40:32 +0200

    annotated an assert

--- core_packages/castle-RPy-writer/pytst/TestDoubles.py
+++ core_packages/castle-RPy-writer/pytst/TestDoubles.py
@@ -12,7 +12,7 @@ def TestDoubles_dir():
     import os;
     dir = Path(os.getcwd())
     while not (dir / TESTDOUBLES).exists():
-        assert str(dir)!=str(dir.parent), f"Can't find {TESTDOUBLES}-dir from {os.getcwd()}"
+        assert str(dir)!=str(dir.parent), f"Can't find {TESTDOUBLES}-dir from {os.getcwd()}" # .f.e. `dir` has become root
         dir = dir.parent
     the_dir = dir / TESTDOUBLES
     assert the_dir.is_dir(), f"found {the_dir}, but it isn't a dir"
