409dc7ace3ad1c567666140836780a0b82aeb80a
Albert Mietus <albert AT mietus DOT nl>
2025-10-19 18:39:31 +0200

    fixed (tmp) logger-levels (INFO->debug)

--- base_packages/castle-monorail/castle/monorail/base/visitors.py
+++ base_packages/castle-monorail/castle/monorail/base/visitors.py
@@ -36,8 +36,7 @@ class Visitor(MRO_Dispatch_Mixin):
             logger.warning("No dispatch found for dispatch_on=%s (prefix=%s), returning empty (%s) -- node=%s",
                                dispatch_on, prefix, empty, node)
             return empty
-        logger.info("Going to call %s for dispatch_on=%s in phase: %s, with node=%s",
-                         method,  dispatch_on, prefix, node) # INFO->DEBUG
+        logger.debug("Going to call %s for dispatch_on=%s in phase: %s, with node=%s", method,  dispatch_on, prefix, node)
         return method(node)
 
     def visit(self, node, dispatch_on=None):
