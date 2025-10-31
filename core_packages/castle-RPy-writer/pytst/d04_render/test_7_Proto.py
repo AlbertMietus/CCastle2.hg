391d29599bd7b8e1ff9ca6960e0b9894e5221c2e
Albert Mietus <albert AT mietus DOT nl>
2025-10-31 19:51:43 +0100

    BUSY: base/_rootProtocol in aigr is GONE

--- core_packages/castle-RPy-writer/pytst/d04_render/test_7_Proto.py
+++ core_packages/castle-RPy-writer/pytst/d04_render/test_7_Proto.py
@@ -3,7 +3,7 @@
 from . import *
 
 
-def test_1_DummyProtocol(my_renderer):
+def test_1a_DummyProtocol_noBase(my_renderer):
     expected = """\
 cc_P_DummyProto = buildin.CC_B_Protocol(name="DummyProto",
     kind = buildin.CC_B_ProtocolKindIs_Event,
@@ -12,5 +12,18 @@ cc_P_DummyProto = buildin.CC_B_Protocol(name="DummyProto",
 \n"""
     proto = aigr.EventProtocol(ID('DummyProto', context=aigr.Def()), events=[], based_on=None) #based_on=None differes from no inheritance!
     txt = my_renderer.render(proto)
+    verify_line_by_line(expected, txt)
+
+
+def test_1b_DummyProtocol_withBase(my_renderer):
+    expected = """\
+cc_P_DummyProto = buildin.CC_B_Protocol(name="DummyProto",
+    kind = buildin.CC_B_ProtocolKindIs_Event,
+    inherit_from = None,
+    events = [])
+\n"""
+    proto = aigr.EventProtocol(ID('DummyProto', context=aigr.Def()), events=[]) # now, .based_on is _RootProtocol
+    txt = my_renderer.render(proto)
+    #print_out(txt)
     verify_line_by_line(expected, txt)
 
