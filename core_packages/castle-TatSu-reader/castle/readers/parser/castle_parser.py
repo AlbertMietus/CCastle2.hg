2ed18a5c5c0362fb5d442c4487814520fa0c9e7b
Albert Mietus <albert AT mietus DOT nl>
2025-12-28 23:29:18 +0100

    cleanup

--- core_packages/castle-TatSu-reader/castle/readers/parser/castle_parser.py
+++ core_packages/castle-TatSu-reader/castle/readers/parser/castle_parser.py
@@ -1,7 +1,11 @@
 # (C) Albert Mietus, 2025. Part of Castle/CCastle project
 
-import tatsu
+import typing as PTH                                       # Python TypeHints
+
 from pathlib import Path
+
+import tatsu
+
 
 from .castle_actions import CastleActions
 
@@ -10,7 +14,7 @@ class CastleParser():
 
     _GRAMMAR_FILE = 'castle_grammar.tatsu'
 
-    def __init__(self, grammar_file:Path=None, actions=None):
+    def __init__(self, grammar_file :PTH.Optional[Path]=None, actions=None):
         if grammar_file is None:
             grammar_file = Path(__file__).parent / self._GRAMMAR_FILE
         with open(grammar_file) as f:
@@ -18,7 +22,7 @@ class CastleParser():
         if actions is None:
             actions = CastleActions
 
-        # HACK for TatSu: absolute file-name is needed for #include
+        # HACK for TatSu: pasing the absolute file-name is needed for `#include`
         self.parser = tatsu.compile(grammar, semantics=CastleActions(), filename=grammar_file.resolve())
 
     def parse(self, text: str, start=None):
