ae92af4ca2cd4a14cefb063dded96b61d198620b
Albert Mietus <albert AT mietus DOT nl>
2025-12-31 17:17:02 +0100

    ToDo: rewriters ('@impliciet....')

---
+++ core_packages/castle-TatSu-reader/pytst/parse/test_99_todo.py
@@ -0,0 +1,15 @@
+# (C) Albert Mietus, 2025- Part of Castle/CCastle project
+import logging; logger = logging.getLogger(__name__)
+import pytest
+
+from castle import aigr
+
+from . import *
+
+@pytest.mark.skip(reason="'@impliciet()' not in the AIGR yet")
+def test_99_rewriter():
+    txt = "@impliciet(Main)"
+    parms = castle_parser(txt, start='rewriter')
+    logger.debug(f"{txt=} ==> {parms=}")
+
+    assert False
