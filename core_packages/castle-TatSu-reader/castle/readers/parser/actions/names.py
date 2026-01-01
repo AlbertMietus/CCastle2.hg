5ed4444b0044fd4c7e021cfaeab5e7574803a294
Albert Mietus <albert AT mietus DOT nl>
2026-01-01 19:16:47 +0100

    support to parse several kind of names (e.g. '.dotted.name' -- a qualID with auto_self)

--- core_packages/castle-TatSu-reader/castle/readers/parser/actions/names.py
+++ core_packages/castle-TatSu-reader/castle/readers/parser/actions/names.py
@@ -5,7 +5,16 @@ from castle import aigr
 
 class Names():
     def nameID(self, ast):
-        return aigr.ID.Def(ast)
+        retval=aigr.ID.Def(ast)
+        logger.info(f"nameID: {ast=} ==> {retval=}")
+        return retval
     def typeID(self, ast):
-        return aigr.ID.Ref(name=ast, context='type')  # XXX HACK
-
+        return aigr.ID.Ref(name=ast, context='type')             # XXX HACK
+    def auto_self(self, ast):
+        retval = aigr.ID.Ref(name=ast, context='self')           # XXX HACK
+        logger.info(f"auto_self: {ast=} ==> {retval=}")
+        return retval
+    def qualID(self, ast):
+        retval = [sub for item in ast for sub in (item if isinstance(item, (list, tuple)) else [item])]
+        logger.info(f"qualID: {ast=} ==> {retval=}")
+        return retval
---
+++ core_packages/castle-TatSu-reader/pytst/parse/test_1a_names.py
@@ -0,0 +1,53 @@
+# (C) Albert Mietus, 2025- Part of Castle/CCastle project
+import logging; logger = logging.getLogger(__name__)
+import pytest
+
+from castle import aigr
+
+from . import *
+
+
+def test_1_aName_as_ID_is_aName(castle_parser):
+    txt="aName"
+    name = castle_parser(txt, start='ID')
+    logger.debug(f"{txt=} ==> {name=} -- {type(name)=}")
+
+    assert isinstance(name, str)
+    assert name == txt
+
+
+def test_2_shortName_as_qualID_is_IDlist(castle_parser):
+    txt="shortName"
+    IDlist = castle_parser(txt, start='qualID')
+    logger.debug(f"{txt=} ==> {IDlist=}")
+
+    verify_ID_list(IDlist, txt.split('.'))
+
+
+def test_3a_dottedName_as_qualID_is_IDlist(castle_parser):
+    txt="dotted.name"
+    IDlist = castle_parser(txt, start='qualID')
+    logger.debug(f"{txt=} ==> {IDlist=}")
+    verify_ID_list(IDlist, txt.split('.'))
+
+def test_3b_dottedName_as_qualID_is_IDlist(castle_parser):
+    txt="self.name"
+    IDlist = castle_parser(txt, start='qualID')
+    logger.debug(f"{txt=} ==> {IDlist=}")
+    verify_ID_list(IDlist, txt.split('.'))
+
+def test_3c_selfDotted_as_qualID_is_IDlist(castle_parser):
+    txt=".name"
+    IDlist = castle_parser(txt, start='qualID')
+    logger.debug(f"{txt=} ==> {IDlist=} -- {type(IDlist)=}")
+    verify_ID_list(IDlist, "self.name".split('.'))
+
+
+
+
+def verify_ID_list(IDlist, names):
+    assert isinstance(IDlist, list),       f"Expecting a list, got: {type(IDlist)=} :: {IDlist}"
+    assert len(IDlist) is len(names),      f"Not the same number of IDs/names: {IDlist} {names}"
+    for i, name in enumerate(IDlist):
+        assert isinstance(name, aigr.ID),  f"part {i} is not an ID: {name}"
+        assert name == names[i],           f"part {i} is wrong name {name=} != {names[i]=} -- {names}"
