b02e31e75e926c297eb2a6a5827af948b2205b99
Albert Mietus <albert AT mietus DOT nl>
2026-01-01 13:03:48 +0100

    cleanup

--- core_packages/castle-TatSu-reader/pytst/parse/test_2_implement.py
+++ core_packages/castle-TatSu-reader/pytst/parse/test_2_implement.py
@@ -14,17 +14,8 @@ implement EmptyComponent
 """
     comp = castle_parser(txt, start='implement_component')
     logger.debug(f"{txt=} ==> {comp=}")
+    verify_ComponentImplementation(comp, name="EmptyComponent")
 
-    assert isinstance(comp, aigr.ComponentImplementation)
-    # direct attributes
-    assert comp.name == "EmptyComponent"
-    assert comp.interface is None
-    assert len(comp.parameters) == 0
-    assert len(comp.handlers) == 0
-    # inherited via _hasScope --|> Scope --|> _NameSpace
-    assert isinstance(comp._ns,      dict)
-    assert isinstance(comp.outer_ns, (dict, type(None))) #.outer_ns is a ref that can be empty ...
-    assert comp.outer_ns is None                         # .. Here it is/should be
 
 def test_2_CompWithParms(castle_parser):
     txt = """\
@@ -34,23 +25,24 @@ implement CompWithParms(p1 :t1, p2 :type2)
 """
     comp = castle_parser(txt, start='implement_component')
     logger.debug(f"{txt=} ==> {comp=}")
-
-    assert isinstance(comp, aigr.ComponentImplementation)
-    # direct attributes
-    assert comp.name == "CompWithParms" #XXX
-    assert comp.interface is None
-    assert len(comp.parameters) == 2    #XXX
-    for parm, name, type_ in zip(       #XXX
+    verify_ComponentImplementation(comp, name="CompWithParms", parameters=2)
+    for parm, name, type_ in zip(
             comp.parameters,
             ('p1', 'p2'),
             ('t1', 'type2')):
         verify_parm(parm, name, type_)
 
 
-    
-    assert len(comp.handlers) == 0
+
+
+def verify_ComponentImplementation(comp, name, parameters=0, handlers=0):
+    assert isinstance(comp, aigr.ComponentImplementation)
+    # direct attributes
+    assert comp.name == name
+    assert comp.interface is None               #XXXX
+    assert len(comp.parameters) == parameters
+    assert len(comp.handlers)   == handlers
     # inherited via _hasScope --|> Scope --|> _NameSpace
     assert isinstance(comp._ns,      dict)
     assert isinstance(comp.outer_ns, (dict, type(None))) #.outer_ns is a ref that can be empty ...
     assert comp.outer_ns is None                         # .. Here it is/should be
-
