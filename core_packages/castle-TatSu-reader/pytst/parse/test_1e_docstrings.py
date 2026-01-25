07d05ca6e6e75e4dc4a84c2e5ae5faba266060cd
Albert Mietus <albert AT mietus DOT nl>
2026-01-25 12:27:34 +0100

    improved (BUSY)

--- core_packages/castle-TatSu-reader/pytst/parse/test_1e_docstrings.py
+++ core_packages/castle-TatSu-reader/pytst/parse/test_1e_docstrings.py
@@ -6,8 +6,20 @@ from castle import aigr
 
 from . import *
 
-def test_1_DocString(castle_parser):
+def test_1a_DocString_1line(castle_parser):
     txt='"""DocMe :-)"""'
     got = castle_parser(txt, start='docstring')
     logger.info(f"{txt=} ==> {got=}")   #XXX debug
     assert isinstance(got, aigr.fString) and got.value == txt[3:-3]
+
+def test_1b_DocString_1line_variants(castle_parser):
+    for txt in (
+        '"""DocMe :-)"""',
+        "'''DocMe :-)'''",
+          '"DocMe :-)"',
+          "'DocMe :-)'" ):
+        got = castle_parser(txt, start='docstring')
+        logger.info(f"{txt=} ==> {got=}")   #XXX debug
+        assert isinstance(got, aigr.fString) and got.value == 'DocMe :-)'
+
+
