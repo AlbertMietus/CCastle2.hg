8e83b34e431918e7c6db5a434ce3774990fec1a1
Albert Mietus <albert AT mietus DOT nl>
2026-01-25 12:41:39 +0100

    many/all variants of docstrings work

--- core_packages/castle-TatSu-reader/pytst/parse/test_1e_docstrings.py
+++ core_packages/castle-TatSu-reader/pytst/parse/test_1e_docstrings.py
@@ -9,7 +9,7 @@ from . import *
 def test_1a_DocString_1line(castle_parser):
     txt='"""DocMe :-)"""'
     got = castle_parser(txt, start='docstring')
-    logger.info(f"{txt=} ==> {got=}")   #XXX debug
+    logger.debug(f"{txt=} ==> {got=}")  
     assert isinstance(got, aigr.fString) and got.value == txt[3:-3]
 
 def test_1b_DocString_1line_variants(castle_parser):
@@ -19,7 +19,25 @@ def test_1b_DocString_1line_variants(castle_parser):
           '"DocMe :-)"',
           "'DocMe :-)'" ):
         got = castle_parser(txt, start='docstring')
-        logger.info(f"{txt=} ==> {got=}")   #XXX debug
+        logger.debug(f"{txt=} ==> {got=}")
         assert isinstance(got, aigr.fString) and got.value == 'DocMe :-)'
 
+def test_2a_DocString_MultiLine(castle_parser):
+    txt='"""\nDocMe\n :-)\n"""'
+    got = castle_parser(txt, start='docstring')
+    logger.debug(f"{txt=} ==> {got=}")
+    assert isinstance(got, aigr.fString) and got.value == txt[3:-3]
 
+def test_2b_DocString_MultiLine_variants(castle_parser):
+    doc="""\
+    More
+    Docs
+    """
+    TD, TS = '"""', "'''"
+    for txt in (
+            TD + doc + TD,
+            TS + doc + TS):
+        got = castle_parser(txt, start='docstring')
+        logger.debug(f"{txt=} ==> {got=}")   #XXX debug
+        assert isinstance(got, aigr.fString) and got.value == doc
+    
