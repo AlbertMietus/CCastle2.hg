3783555b96442c8d8374000b6d00f92bab83d4d4
Albert Mietus <albert AT mietus DOT nl>
2026-01-28 20:30:24 +0100

    refactor/cleanup

--- core_packages/castle-TatSu-reader/pytst/HW/test_1_elemental.py
+++ core_packages/castle-TatSu-reader/pytst/HW/test_1_elemental.py
@@ -10,24 +10,13 @@ from . import *
 
 
 @pytest.mark.xfail(reason="Dot-on-Horizon: elemental HelloWorld")
-def test_1_file(castle_parser):
-    """.. todo::
-
-          * ```HelloWorld(label :string) { 			///GAM: fixed `name :type` order
-                   print("Hello {label} World")
-               }```
-
-    """
+def test_1_file_as_txt(castle_parser):
     module, file  = "CastleCode.elemental", "HelloWorld.Castle"
     with resources.open_text( module, file) as f:
         logger.debug("Going to read %s", f.name)
         txt = f.read()
-    if False:
-        print(f"\n\n====={module}::{file}=====")
-        print(txt)
-        print("=====")
 
     eHW = castle_parser(txt)
     pprint(eHW)
 
-    assert False, "Work to do -- but it parses!!"
+    assert False, "More tests are needed -- but it parses!!"
