ec1b5b690e59b6e084afcd484953c10bf2a44959
Albert Mietus <albert AT mietus DOT nl>
2026-03-23 15:51:18 +0100

    cleanup

--- core_packages/castle-TatSu-reader/pytst/d01_parse/d06_meta/test_99_todo.py
+++ core_packages/castle-TatSu-reader/pytst/d01_parse/d06_meta/test_99_todo.py
@@ -6,8 +6,8 @@ from castle import aigr
 
 from . import *
 
-@pytest.mark.skip(reason="'@impliciet()' not in the AIGR yet")
-def test_99_rewriter():
+#@pytest.mark.skip(reason="'@impliciet()' not in the AIGR yet")
+def test_99_rewriter(castle_parser):
     txt = "@impliciet(Main)"
     parms = castle_parser(txt, start='rewriter')
     logger.debug(f"{txt=} ==> {parms=}")
