da0a7a7740b40288e1f11b68e9a33f2164e04312
Albert Mietus <albert AT mietus DOT nl>
2026-04-09 10:36:56 +0200

    cleanup

--- core_packages/castle-RPy-writer/pytst/d03_RPy/machinery/test_0_Bundle.py
+++ core_packages/castle-RPy-writer/pytst/d03_RPy/machinery/test_0_Bundle.py
@@ -8,9 +8,20 @@ from castle.writers.RPy.aid import Block
 
 from .fixtures import bundler
 
+class NonBundlerStub(Bundler):
+    """When pack/unpack are not implemented, those function should raise 'NotImplementedError'
+       However, we need a trick (see below) to make sure we can instance NonBundlerStub"""
+    def __new__(cls, hint:str="", **kwargs):
+        return object.__new__(cls)
+    def pack(self, *t, **kw):
+        return super().pack(*t,**kw)                                     # type: ignore # we need this trick to test ...
+    def unpack(self, *t, **kw):
+        return super().unpack(*t,**kw)                                   # type: ignore # we need this trick to test ...
+
 
 def test_0a_Bundler_is_NativeBundler(bundler):
     assert isinstance(bundler, NativeBundler)
+
 
 def test_0b_pack_and_unpack_return_TextBlock(bundler):
     txt = bundler.pack(arguments=(), signature=())
@@ -18,15 +29,6 @@ def test_0b_pack_and_unpack_return_TextBlock(bundler):
     txt = bundler.unpack(signature=())
     assert isinstance(txt, (str, Block))
 
-class NonBundlerStub(Bundler):
-    """When pack/unpack are not implemented, those function should raise 'NotImplementedError'
-       However, we need a trick (see below) to make sure we can instance NonBundlerStub"""
-    def __new__(cls, hint:str="", **kwargs):
-        return object.__new__(cls)
-    def pack(self, *t, **kw):
-        return super().pack(*t,**kw)      # type: ignore # we need this trick to test ...
-    def unpack(self, *t, **kw):
-        return super().unpack(*t,**kw)    # type: ignore # we need this trick to test ...
 
 def test_0c_abstractmethods_raises():
     b = NonBundlerStub()
