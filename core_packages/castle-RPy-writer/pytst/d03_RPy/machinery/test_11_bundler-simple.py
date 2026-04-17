8e421b0604662c4b3bcf4655a69a399f5363ce99
Albert Mietus <albert AT mietus DOT nl>
2026-04-17 11:13:49 +0200

    Test to pack_some_positional_ints works

--- core_packages/castle-RPy-writer/pytst/d03_RPy/machinery/test_11_bundler-simple.py
+++ core_packages/castle-RPy-writer/pytst/d03_RPy/machinery/test_11_bundler-simple.py
@@ -8,11 +8,8 @@ from .verify import *
 from castle import aigr
 from castle.aigr import types as CCTypes
 
-def test_1_pack_single_positional_int(bundler):
-    arguments = ["1"]
-    formal_parameters: aigr.OptionalTypedParameterList =(aigr.TypedParameter(name='_dummy', type=CCTypes.int),)
-    expected = '[CC_B_int(1)], {}'
 
+def box_and_pack(bundler, arguments, formal_parameters) ->str:
     boxed: list[str] = []
     for arg, parm in zip(arguments, formal_parameters):
         typ = parm.type
@@ -20,8 +17,32 @@ def test_1_pack_single_positional_int(bundler):
         logger.info(f"bundler.box :: {arg=} + {typ=} ==> {type_tag=}")
         boxed.append(type_tag)
     logger.info(f"{boxed=}")
+    return bundler.pack(boxed, formal_parameters)
 
-    txt: str = bundler.pack(boxed, formal_parameters)
+
+def test_1_pack_single_positional_int(bundler):
+    arguments = ["1"]
+    formal_parameters: aigr.OptionalTypedParameterList =(aigr.TypedParameter(name='_dummy', type=CCTypes.int),)
+    expected = '[CC_B_int(1)], {}'
+
+    txt=box_and_pack(bundler, arguments, formal_parameters)
+
     assert str(txt) == expected, f"Got {txt=}, when packing {arguments=} -- {expected=}"
     verify_ValidPython(txt)
 
+
+def test_2_pack_some_positional_ints(bundler):
+    arguments = ["1", "3/2", "42"]
+    formal_parameters: aigr.OptionalTypedParameterList = (
+        aigr.TypedParameter(name='_dummy', type=CCTypes.int),
+        aigr.TypedParameter(name='_dummy', type=CCTypes.int),
+        aigr.TypedParameter(name='_dummy', type=CCTypes.int),)
+    expected = '[' + (
+        'CC_B_int(1), ' +
+        'CC_B_int(3/2), ' +
+        'CC_B_int(42)' ) + '], {}'
+
+    txt=box_and_pack(bundler, arguments, formal_parameters)
+
+    assert str(txt) == expected, f"Got {txt=}, when packing {arguments=} -- {expected=}"
+    verify_ValidPython(txt)
